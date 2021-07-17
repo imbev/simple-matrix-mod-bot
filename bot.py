c87ac1563e332da4e8c2905d7adb1ebeb176df20
krazykirby99999 <krazykirby99999@gmail.com>
2021-07-17 18:40:28 +0000

    Set session storage to .storage/session.txt

--- bot.py
+++ bot.py
@@ -3,8 +3,11 @@ import os
 
 import dotenv; dotenv.load_dotenv()
 
+local_storage_path = "./.storage/"
+
 creds = botlib.Creds(
     os.getenv("HOMESERVER"),
     os.getenv("USERNAME"),
-    os.getenv("PASSWORD")
+    os.getenv("PASSWORD"),
+    f"{local_storage_path}/session.txt"
 )
