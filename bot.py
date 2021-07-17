ecd4044877f9dcd91786e3630f31e59d5ec5b6b6
KrazyKirby99999 <69373044+KrazyKirby99999@users.noreply.github.com>
2021-07-17 15:15:49 -0500

    Add bot functionality (#4)

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import simplematrixbotlib as botlib
 import os
+import modules
 
 import dotenv; dotenv.load_dotenv()
 
-local_storage_path = "./.storage/"
+local_storage_path = os.getenv("STORAGE")
+if not os.path.isdir(local_storage_path):
+    os.mkdir(local_storage_path)
 
 creds = botlib.Creds(
     os.getenv("HOMESERVER"),
@@ -11,3 +14,16 @@ creds = botlib.Creds(
     os.getenv("PASSWORD"),
     f"{local_storage_path}/session.txt"
 )
+
+bot = botlib.Bot(creds)
+
+bot.prefix = os.getenv("PREFIX")
+
+admin_ids = []
+
+bot_modules = [
+    modules.BlockString(admin_ids, bot, f"{local_storage_path}/string_blocklist.txt")
+]
+
+
+bot.run()
